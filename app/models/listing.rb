64141004b8d4e097dc16d4e302b9de719a590bb1
Michael Selasi <sdzamesi@gmail.com>
2017-06-02 13:50:23 +0000

    Figaro and Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,14 +1,14 @@
 class Listing < ApplicationRecord
 	if Rails.env.development?
-	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg", validate_media_type: false
-else
-	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg", validate_media_type: false
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
-  	#validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
-	   	:storage => :dropbox,
-	    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	    #:path => ":style/:id_:filename"
-end
+		has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg", validate_media_type: false
+	else
+		has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg", validate_media_type: false
+	  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/,
+	  	#validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+		   	:storage => :dropbox,
+		    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+		    :path => ":style/:id_:filename"
+	end
 validates :name, :description, :price, presence: true
 validates :price, numericality: {greater_than: 0}
 validates_attachment_presence :image
